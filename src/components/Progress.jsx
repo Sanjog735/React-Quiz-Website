e8fb418204565b741d4ffca8e62e4c65fbe46f79
Sanjog Patel <61593168+Sanjog735@users.noreply.github.com>
2024-06-13 07:25:42 +0530

    Update Progress.jsx

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Progress = ({ index, numQuestions, points, answer, maxPoints }) => {
   return (
-    <header className="progress px-[20%] md:px-0 items-center text-xl md:text-3xl">
+    <header className="progress  items-center text-xl md:text-3xl">
       <progress max={numQuestions} value={index + Number(answer !== null)} />
       <p>
         Question <strong>{index + 1}</strong> / {numQuestions}
